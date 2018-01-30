d01199859b1575ae447219361b3eaad84cf46cb1
Ryan Andersen <ryan.andersen05@gmail.com>
2018-01-30 12:49:05 -0500

    MovementMechanics script added to project

---
+++ Assets/Scripts/GameMechanics/MovementMechanics.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementMechanics : MonoBehaviour {
+
+    #region main variables
+    public float maxRunSpeed = 20f;
+    public float maxWalkSpeed = 9f;
+    #endregion main variables
+
+    #region set at runtime
+    public float horizontalInput { get; set; }
+    public float verticalInput { get; set; }
+    #endregion set at runtime
+
+    #region monobehaviour methods
+    private void Update()
+    {
+
+    }
+    #endregion monobehaviour methos
+}
