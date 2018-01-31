5ace82977e066870e18f410162afdbd51f3df200
Ryan Andersen <ryan.andersen05@gmail.com>
2018-01-31 02:55:31 -0500

    Bullet animations added in

---
+++ Assets/Scripts/ProjectileScripts/Projectile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    #region main variables
+    public float lauchSpeed = 10;
+    public Vector2 launchVector = Vector2.right;
+    #endregion main variables
+
+    #region monobehaviour methods
+
+    #endregion monobehaviour methods
+}
