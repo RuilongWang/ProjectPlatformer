bf6a50d419b24240eea4d4c25a7d581557c62134
Ryan Andersen <ryan.andersen05@gmail.com>
2018-02-01 00:32:04 -0500

    Added block for a charater spirtre

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+    public float maxHealth = 100;
+    public float currentHealth { get; protected set; }
+
+    public void TakeDamage(float damageTaken)
+    {
+
+    }
+}
---
+++ Assets/Scripts/GameOverseer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Keeps track of important references in the game
+/// </summary>
+public class GameOverseer : MonoBehaviour {
+    public PlayerController player;
+
+
+}
