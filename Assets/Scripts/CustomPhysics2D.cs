2d8ab6c69ab43d79bbe4986264eed4c2a9b74f67
Ryan Andersen <ryan.andersen05@gmail.com>
2018-01-31 22:52:53 -0500

    Very basic physics 2d script

---
+++ Assets/Scripts/CustomPhysics2D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomPhysics2D : MonoBehaviour {
+    public Vector2 velocity { get; set; }
+
+
+    #region monobehavoiur methods
+    private void Update()
+    {
+        UpdatePositionFromVelocity();
+    }
+    #endregion monobehaviour methods
+
+
+    private void UpdatePositionFromVelocity()
+    {
+        Vector3 velocityVector3 = new Vector3(velocity.x, velocity.y, 0);
+        this.transform.position += velocityVector3 * Time.deltaTime;
+    }
+}
