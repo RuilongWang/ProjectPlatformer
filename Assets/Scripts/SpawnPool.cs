a0e42cd2de7dfd3f12a12f5966d859aee96a0491
Ryan Andersen <ryan.andersen05@gmail.com>
2018-03-01 02:41:38 -0500

    spawn pool started

---
+++ Assets/Scripts/SpawnPool.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPool <T> : MonoBehaviour where T : MonoBehaviour  {
+    public T[] listOfObjectToPool;
+    public int initialAmountToPoolPerObject = 10;
+
+    private Queue<T> queuedObjectsInPool = new Queue<T>();
+
+    #region Monobehaviour methods
+    private void Awake()
+    {
+
+    }
+
+
+    
+    #endregion monobehaviour methods
+
+    public void Spawn(T prefabToSpawn)
+    {
+
+    }
+
+    public void Despawn(T objectToDespawn)
+    {
+
+    }
+
+    protected virtual T CreateNewObjectForSpawnPool(T prefabToPool)
+    {
+        return Instantiate<T>(prefabToPool);
+    }
+}
