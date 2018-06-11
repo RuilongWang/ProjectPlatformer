df0f63199c9a7c22933b0e0f0b315c8f36697b78
Ryan Andersen <ryan.andersen05@gmail.com>
2018-06-10 22:57:44 -0400

    starting attack mechanics

---
+++ Assets/Scripts/GameMechanics/AttackMechanics.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackMechanics : MonoBehaviour {
+    #region main variables
+
+
+    private Animator anim;
+    #endregion main variables
+
+    #region monobehaviour methods
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    #endregion monobehaviour methods
+
+    private void UseMeleeAttack()
+    {
+
+    }
+}
