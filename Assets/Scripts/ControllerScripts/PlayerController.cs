0f67a39b5ae84f36b577a0348f10d0b6b9fe98e9
Ryan Andersen <ryan.andersen05@gmail.com>
2020-04-24 11:40:32 -0400

    Adding a player controller script to game

--- Assets/Scripts/ControllerScripts/PlayerController.cs
+++ Assets/Scripts/ControllerScripts/PlayerController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,4 +13,64 @@ public class PlayerController : MonoBehaviour
     public const string HORIZONTAL_AXIS = "MoveLeftRight";
     public const string VERTICAL_AXIS = "MoveUpDown";
     #endregion const variables
+
+    private Dictionary<string, UnityAction> ActionEventDictioanry = new Dictionary<string, UnityAction>();
+    private Dictionary<string, UnityAction<float>> AxisEventDictionary = new Dictionary<string, UnityAction<float>>();
+
+
+    private void Update()
+    {
+
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="InputName"></param>
+    /// <param name="IsPressedEvent"></param>
+    /// <param name="FunctionToPerform"></param>
+    public void BindInputToAction(string InputName, bool IsPressedEvent, Action FunctionToPerform)
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="InputName"></param>
+    /// <param name="IsPressedEvent"></param>
+    public void UnbindInputAction(string InputName, bool IsPressedEvent)
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="AxisName"></param>
+    /// <param name="FunctionToPerform"></param>
+    public void BindAxisToAction(string AxisName, Action<float> FunctionToPerform)
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="AxisName"></param>
+    public void UnbindActionFromAxis(string AxisName)
+    {
+
+    }
+
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    private class ActionEventInfo
+    {
+        public string InputName;
+        public bool IsThisAPressedEvent;//If not we assume its a release event
+    }
 }
